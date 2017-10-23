428886bc58aedbbe2c7817830721e814680da2d3
autozcoursera <autolordz@gmail.com>
2017-10-23 18:07:20 +0800

    add plot assignments 2

--- plot4.R
+++ plot4.R
@@ -10,8 +10,11 @@ dat_EPC_sub <-subset(dat_EPC,Date >= as.Date("2007-2-1") & Date <= as.Date("2007
 
 #No.4
 par(mfrow = c(2,2),mar=c(4,4,2,2), oma=c(2,2,0,2))
+#plot1
 with(dat_EPC_sub,plot(SetTime,Global_active_power,type = "l",xlab = "",ylab = "Global Active Power"))
+#plot2
 with(dat_EPC_sub,plot(SetTime,Voltage,type = "l",xlab = "datetime",ylab = "Voltage"))
+#plot3
 with(dat_EPC_sub,{
   plot(SetTime,Sub_metering_1, type="l",
        ylab="Energy sub metering", xlab="")
@@ -19,7 +22,8 @@ with(dat_EPC_sub,{
   lines(SetTime,Sub_metering_3,col='Blue')
 })
 legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1),bty = "n",
-       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),cex = 0.5)
+#plot4
 with(dat_EPC_sub,plot(SetTime,Global_reactive_power,type = "l",xlab = "datetime",
                       ylab = "Global_reactive_power"))
 
